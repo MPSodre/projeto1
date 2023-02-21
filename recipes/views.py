de7bf8770b3c6731ba97e80e75116cca0a5a5739
Marcos Sodré <sodre22@gmail.com>
2023-02-21 16:27:17 -0300

    mensagem

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Marcos',
+    })
+
+
+def contato(request):
+    return render(request, 'temp.html')
+
+
+def sobre(request):
+    return HttpResponse('sobre1')
